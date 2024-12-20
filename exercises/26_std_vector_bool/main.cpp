5129255ecf841cc3abcb77e69564fddd071e179b
cruise1230 <18831441271@163.com>
2024-12-20 21:23:04 +0800

    learning-cxx

--- exercises/26_std_vector_bool/main.cpp
+++ exercises/26_std_vector_bool/main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char **argv) {
     ASSERT(vec[99], "Make this assertion pass.");
     ASSERT(vec.size() == 100, "Make this assertion pass.");
     // NOTICE: 平台相关！注意 CI:Ubuntu 上的值。
-    std::cout << "sizeof(std::vector<bool>) = " << sizeof(std::vector<bool>) << std::endl;
-    ASSERT(sizeof(vec) == 13, "Fill in the correct value.");
+    std::cout << "sizeof(std::vector<bool>) = " << sizeof(vec) << std::endl;
+    ASSERT(sizeof(vec) == 32, "Fill in the correct value.");
     {
         vec[20] = false;
         ASSERT(!vec[20], "Fill in `vec[20]` or `!vec[20]`.");
